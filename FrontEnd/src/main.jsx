b1207934741f7084e472203fb4632b1422537eab
Mr-Thanush <thanush.2645@gmail.com>
2026-02-04 17:44:15 +0530

    bug fix

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
-import { AuthProvider } from '../components/authContext'
-import { CartProvider } from '../components/cartContext'
-import { WishlistProvider } from '../components/wishlistContext'
+import { AuthProvider } from './components/authContext'
+import { CartProvider } from './components/cartContext'
+import { WishlistProvider } from './components/wishlistContext'
 import './index.css'
 import App from './App'
 
