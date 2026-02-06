6d36e05671d198578a14aa9dc3042f4f868f9d49
Mr-Thanush <thanush.2645@gmail.com>
2026-02-06 20:16:57 +0530

    signin bug3

--- BackEnd/Models/userModel.js
+++ BackEnd/Models/userModel.js
@@ -1,75 +1,87 @@
 import mongoose from "mongoose";
-import validator from 'validator';
-import bcryptjs from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import crypto from 'crypto'
+import validator from "validator";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Please Enter your name"],
-        maxLength: [25, "Invalid name. Please enter a name with fewer than 25 character"],
-        minLength: [3, "Name should contain more than 3 characters"]
-
+      type: String,
+      required: [true, "Please enter your name"],
+      maxLength: [25, "Name must be less than 25 characters"],
+      minLength: [3, "Name must be at least 3 characters"],
     },
+
     email: {
-        type: String,
-        required: [true, "Please Enter your email"],
-        unique: true,
-        validate: [validator.isEmail, "Please enter valid email"]
+      type: String,
+      required: [true, "Please enter your email"],
+      unique: true,
+      validate: [validator.isEmail, "Please enter a valid email"],
+    },
 
-    },
     password: {
-        type: String,
-        required: [true, "Please Enter your Password"],
-        minLength: [8, "Password should be greater than 8 character"],
-        select: false
+      type: String,
+      required: [true, "Please enter your password"],
+      minLength: [8, "Password must be at least 8 characters"],
+      select: false,
     },
+
     role: {
-        type: String,
-        enum: ["user", "seller", "admin"],
-        default: "user"
+      type: String,
+      enum: ["user", "seller", "admin"],
+      default: "user",
     },
+
     hasStore: {
-  type: Boolean,
-  default: false,
-},
+      type: Boolean,
+      default: false,
+    },
 
     resetPasswordToken: String,
-    resetPasswordExpire: Date
+    resetPasswordExpire: Date,
+  },
+  { timestamps: true }
+);
 
+//PASSWORD HASHING
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
 
-
-}, { timestamps: true })
-
-//Password Hashing
-userSchema.pre("save", async function () {
-    // If password is NOT modified, move on
-    if (!this.isModified("password")) {
-        return ;
-    }
-
-    // Hash ONLY when password changes
-    this.password = await bcryptjs.hash(this.password, 10);
-   
+  this.password = await bcryptjs.hash(this.password, 10);
+  next();
 });
-userSchema.methods.getJWTToken = function (){
-    return jwt.sign({ id: this._id },process.env.JWT_SECRET_KEY,{
-        expiresIn:process.env.JWT_EXPIRE
-    })
-}
 
-userSchema.methods.passVerification =async function(userEnteredPass){
-    return await bcryptjs.compare(userEnteredPass,this.password);
-}
+//COMPARE PASSWORD
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcryptjs.compare(enteredPassword, this.password);
+};
 
-//generate token
-userSchema.methods.generatePassResetToken=function(){
-    const resetToken=crypto.randomBytes(20).toString("hex");
-    this.resetPasswordToken=crypto.createHash("sha256").update(resetToken).digest("hex");
-    this.resetPasswordExpire=Date.now()+30*60*1000 //30min
-    return resetToken;
+//JWT TOKEN
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign(
+    { id: this._id },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: process.env.JWT_EXPIRE,
+    }
+  );
+};
 
-}
+// RESET PASSWORD TOKEN
+userSchema.methods.generatePassResetToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
 
-export default mongoose.model("User", userSchema)
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 30 * 60 * 1000; // 30 minutes
+
+  return resetToken;
+};
+
+export default mongoose.model("User", userSchema);
